568f8a7a4c4f98affb5a1161d8a21b94fedc0c30
sairasiddique05 <sairasiddique05@gmail.com>
2025-11-20 12:35:28 +0500

    Fix vite.config.js import for defineConfig

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+import { defineConfig } from 'vite'    // ✅ make sure this line exists
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './', // ensures paths are relative
